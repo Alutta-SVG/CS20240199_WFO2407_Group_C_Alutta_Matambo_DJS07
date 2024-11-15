bf93614af1c9961479b8c38e81597895866af6ee
Alutta <Aluttamatambo5@gmail.com>
2024-11-15 12:50:51 +0200

    Added React component structure and form input

---
+++ Meme.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Meme() {
+    return (
+        <main>
+            <form>
+                <div>
+                    <input>
+                    </input>
+                </div>
+            </form>
+        </main>
+    )
+}
