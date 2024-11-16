df38c768559018e3e1c1d7e22e6b45622644b529
Alutta <Aluttamatambo5@gmail.com>
2024-11-16 14:17:48 +0200

    Refactor App component import statement fixed, state added, form and button elements updated, and meme image rendering added

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,39 @@
 import React from "react";
-import memesData from"../memesData.js"
-function App() {
- let url
+import memesData from "../memesData.js";
+
+export default function App() {
+  const [memeImage, setMemeImage] = React.useState("");
 
   function getMemeImage() {
-    const memesArray = memesData.data.memes
-    const randomNumber = Math.floor(Math.random() * memesArray.length)
-    url = memesArray[randomNumber].url
-    console.log(url)
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
   }
+
   return (
     <main>
-    <form className="form">
-        <div>
-            <label htmlFor="top-text">Top Text</label>
-            <input
-                id="top-text"
-                type="text"
-                placeholder="Shut up"
-                className="form--input"
-            />
-        </div>
-        <div>
-            <label>Bottom Text
-            <input
-                    type="text"
-                    placeholder="and take my money"
-                    className="form--input"
-                />
-            </label>
-        </div>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Top text"
+          className="form--input"
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="form--input"
+        />
+        
         <button
-            className="form--button"
+          className="form--button"
+          onClick={getMemeImage}
         >
-            Get a new meme image 🖼
+          Get a new meme image 🖼
         </button>
-    </form>
-</main>
-)
+      </div>
+      {memeImage && <img src={memeImage} className="meme--image" alt="Meme" />}
+    </main>
+  );
+
 }
 
-export default App
