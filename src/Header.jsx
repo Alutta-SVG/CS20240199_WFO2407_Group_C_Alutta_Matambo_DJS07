d2c81d59945a7c8dea476bcb6cf372c07265519c
Alutta <Aluttamatambo5@gmail.com>
2024-11-17 16:29:44 +0200

    Updated Header component and index.css to add a container div, adjust styles and layout

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,23 @@
+import React from 'react';
 export default function Header() {
+    
     return (
         <header className="header">
             {/* Use /vite.svg if it's in the public folder */}
-            <img 
+           
+           <div className="header-container">
+           <img
                 src="/vite.svg"
                 className="header-image"
-                alt="Vite Logo"  // Always include alt text for accessibility
+                alt="Vite Logo"
             />
-            <h2 className="header-title">Meme Generator</h2> 
-            <h4 className="header-project">React Project - DJS07</h4>
+
+    <h2 className="header-title">Meme Generator</h2>
+
+    <h4 className="header-project">React Project - DJS07</h4>
+</div>
+
+
         </header>
     );
 }
